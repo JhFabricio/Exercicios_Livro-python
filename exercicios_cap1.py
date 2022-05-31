667a666e002346d6e0be70b135cec0dfc14892b7
Jhonata Fabricio <97449786+JhFabricio@users.noreply.github.com>
2022-05-31 15:44:04 -0300

    Commit Inicial
    Commit em que eu adicionei as variáveis, efetuei os cálculos e imprimi o resultado final do primeiro exercício

---
+++ exercicios_cap1.py
@@ -0,0 +1,12 @@
+#Enunciado: 1. Quantos segundos há em 42 minutos e 42 segundos?
+
+#Variáveis
+minutos = 42
+segundos = 42
+
+#Cálculos
+minutos_convertidos = minutos * 60
+total = minutos_convertidos + segundos
+
+#Resposta final
+print(f'{minutos} minutos e {segundos} segundos são equivalentes a {total} segundos')
