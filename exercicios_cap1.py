7cd60e0ecdf297d8b9eedf9e1ef30a06471e5cb4
Jhonata Fabricio <97449786+JhFabricio@users.noreply.github.com>
2022-06-01 13:33:55 -0300

    Commit 2
    Resolução do exercício 2, incluindo a adição das variáveis e a efetuação dos cálculos

--- exercicios_cap1.py
+++ exercicios_cap1.py
@@ -10,3 +10,15 @@ total = minutos_convertidos + segundos
 
 #Resposta final
 print(f'{minutos} minutos e {segundos} segundos são equivalentes a {total} segundos')
+
+#--/--/--/--
+
+#Enunciado: 2. Quantas milhas há em 10 quilômetros? 
+#Dica: uma milha equivale a 1,61 quilômetro.
+
+#Variáveis
+km = 10
+milhas = round(km * 0.62137, 2)
+
+#Resposta final
+print(f'Há {milhas} milhas em {km} km')
